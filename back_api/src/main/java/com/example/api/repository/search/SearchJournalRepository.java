36e211a8b87d4fb7c38ab2924431f4e9e7fc668a
limeorangenamu <scy7219@gmail.com>
2026-08-21 12:40:28 +0900

    저널 검색 메서드 호환성 복구

--- back_api/src/main/java/com/example/api/repository/search/SearchJournalRepository.java
+++ back_api/src/main/java/com/example/api/repository/search/SearchJournalRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface SearchJournalRepository {
+    default Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
+        return searchPage(type, keyword, pageable, null, null);
+    }
+
     Page<Object[]> searchPage(String type, String keyword, Pageable pageable, Long mid, Boolean isPublic);
 }
